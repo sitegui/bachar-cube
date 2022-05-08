126093ebf54aca897974bdc470018c65005f6528
sitegui <sitegui@sitegui.com.br>
2022-05-08 23:17:23 +0200

    Faster

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 use position::Position;
-use std::cmp::Ordering;
 use std::collections::{HashSet, VecDeque};
 use std::fmt;
 use std::time::Instant;
@@ -9,223 +8,89 @@ mod position;
 
 /// Represent the pieces that can be in the top or bottom layers
 #[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
-enum OuterPieceName {
+#[repr(u8)]
+enum OuterPiece {
     // Top
-    WhiteRedBlue,
-    WhiteBlue,
-    WhiteBlueOrange,
-    WhiteOrange,
-    WhiteOrangeGreen,
-    WhiteGreen,
-    WhiteGreenRed,
-    WhiteRed,
+    WhiteRedBlue1 = 0,
+    WhiteRedBlue2 = 1,
+    WhiteBlue = 2,
+    WhiteBlueOrange1 = 4,
+    WhiteBlueOrange2 = 5,
+    WhiteOrange = 6,
+    WhiteOrangeGreen1 = 8,
+    WhiteOrangeGreen2 = 9,
+    WhiteGreen = 10,
+    WhiteGreenRed1 = 12,
+    WhiteGreenRed2 = 13,
+    WhiteRed = 14,
     // Bottom
-    YellowOrange,
-    YellowOrangeBlue,
-    YellowBlue,
-    YellowBlueRed,
-    YellowRed,
-    YellowRedGreen,
-    YellowGreen,
-    YellowGreenOrange,
+    YellowOrangeBlue1 = 16,
+    YellowOrangeBlue2 = 17,
+    YellowBlue = 18,
+    YellowBlueRed1 = 20,
+    YellowBlueRed2 = 21,
+    YellowRed = 22,
+    YellowRedGreen1 = 24,
+    YellowRedGreen2 = 25,
+    YellowGreen = 26,
+    YellowGreenOrange1 = 28,
+    YellowGreenOrange2 = 29,
+    YellowOrange = 30,
 
-    Empty,
+    Unknown = 255,
 }
-
-#[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
-struct OuterPiece {
-    name: OuterPieceName,
-    size: u8,
-}
-
-#[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
-pub enum MiddleLayer {
-    Solved,
-    Inverted,
-}
-
-/// Invariants:
-/// - the sum of the first `num_pieces` sizes is 12
-/// - the sum of the first `movable_pieces` sizes is 6
-/// - no repeated element
-#[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
-pub struct OuterLayer {
-    /// A layer is made of at least 6 pieces (6 big) or 10 pieces (2 big, 8 small).
-    pieces: [OuterPiece; OUTER_LAYER_MAX_PIECES],
-    num_pieces: usize,
-    movable_pieces: usize,
-}
-
-const OUTER_LAYER_MAX_PIECES: usize = 10;
-const OUTER_LAYER_TOTAL_SIZE: u8 = 12;
-const OUTER_LAYER_HALF_SIZE: u8 = 6;
 
 impl OuterPiece {
-    const WHITE_RED_BLUE: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteRedBlue,
-        size: 2,
-    };
-    const WHITE_BLUE: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteBlue,
-        size: 1,
-    };
-    const WHITE_BLUE_ORANGE: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteBlueOrange,
-        size: 2,
-    };
-    const WHITE_ORANGE: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteOrange,
-        size: 1,
-    };
-    const WHITE_ORANGE_GREEN: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteOrangeGreen,
-        size: 2,
-    };
-    const WHITE_GREEN: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteGreen,
-        size: 1,
-    };
-    const WHITE_GREEN_RED: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteGreenRed,
-        size: 2,
-    };
-    const WHITE_RED: OuterPiece = OuterPiece {
-        name: OuterPieceName::WhiteRed,
-        size: 1,
-    };
-    const YELLOW_ORANGE: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowOrange,
-        size: 1,
-    };
-    const YELLOW_ORANGE_BLUE: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowOrangeBlue,
-        size: 2,
-    };
-    const YELLOW_BLUE: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowBlue,
-        size: 1,
-    };
-    const YELLOW_BLUE_RED: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowBlueRed,
-        size: 2,
-    };
-    const YELLOW_RED: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowRed,
-        size: 1,
-    };
-    const YELLOW_RED_GREEN: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowRedGreen,
-        size: 2,
-    };
-    const YELLOW_GREEN: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowGreen,
-        size: 1,
-    };
-    const YELLOW_GREEN_ORANGE: OuterPiece = OuterPiece {
-        name: OuterPieceName::YellowGreenOrange,
-        size: 2,
-    };
-    const EMPTY: OuterPiece = OuterPiece {
-        name: OuterPieceName::Empty,
-        size: 0,
-    };
-}
-
-impl MiddleLayer {
-    fn flip(self) -> MiddleLayer {
-        match self {
-            MiddleLayer::Solved => MiddleLayer::Inverted,
-            MiddleLayer::Inverted => MiddleLayer::Solved,
-        }
+    fn can_split_before(self) -> bool {
+        self as u8 % 2 == 0
     }
 }
 
+#[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
+pub struct OuterLayer {
+    pieces: [OuterPiece; OUTER_LAYER_PIECES],
+}
+
+const OUTER_LAYER_PIECES: usize = 12;
+const OUTER_LAYER_HALF_PIECES: usize = 6;
+
 impl OuterLayer {
-    fn new(pieces: &[OuterPiece]) -> Self {
-        assert!(pieces.len() <= OUTER_LAYER_MAX_PIECES);
+    fn new(pieces: [OuterPiece; 12]) -> Self {
+        assert!(pieces[0].can_split_before());
+        assert!(pieces[OUTER_LAYER_HALF_PIECES].can_split_before());
 
-        let mut movable_pieces = None;
-        let mut total_size = 0;
-        for (i, &piece) in pieces.iter().enumerate() {
-            if total_size == OUTER_LAYER_HALF_SIZE {
-                movable_pieces = Some(i);
-            }
-            total_size += piece.size;
-        }
-
-        assert_eq!(total_size, OUTER_LAYER_TOTAL_SIZE);
-
-        let mut array_pieces = [OuterPiece::EMPTY; OUTER_LAYER_MAX_PIECES];
-        array_pieces[..pieces.len()].copy_from_slice(pieces);
-
-        OuterLayer {
-            pieces: array_pieces,
-            num_pieces: pieces.len(),
-            movable_pieces: movable_pieces.unwrap(),
-        }
+        OuterLayer { pieces }
     }
 
     fn for_each_movement(self, mut f: impl FnMut(Self)) {
-        for shift in 1..self.num_pieces {
-            let mut i = shift;
-            let mut size = 0;
-            let mut new_movable_pieces = 0;
-            loop {
-                size += self.pieces[i].size;
-                new_movable_pieces += 1;
+        for shift in 1..OUTER_LAYER_PIECES {
+            let new_cut = (shift + OUTER_LAYER_HALF_PIECES) % OUTER_LAYER_PIECES;
 
-                match size.cmp(&OUTER_LAYER_HALF_SIZE) {
-                    Ordering::Less => {}
-                    Ordering::Equal => {
-                        let mut new_pieces = [OuterPiece::EMPTY; OUTER_LAYER_MAX_PIECES];
-                        new_pieces[..self.num_pieces - shift]
-                            .copy_from_slice(&self.pieces[shift..self.num_pieces]);
-                        new_pieces[self.num_pieces - shift..self.num_pieces]
-                            .copy_from_slice(&self.pieces[..shift]);
-                        f(OuterLayer {
-                            pieces: new_pieces,
-                            num_pieces: self.num_pieces,
-                            movable_pieces: new_movable_pieces,
-                        });
-                    }
-                    Ordering::Greater => {
-                        break;
-                    }
-                }
-
-                i = (i + 1) % self.num_pieces;
+            if self.pieces[shift].can_split_before() && self.pieces[new_cut].can_split_before() {
+                let mut new_pieces = [OuterPiece::Unknown; OUTER_LAYER_PIECES];
+                new_pieces[..OUTER_LAYER_PIECES - shift].copy_from_slice(&self.pieces[shift..]);
+                new_pieces[OUTER_LAYER_PIECES - shift..].copy_from_slice(&self.pieces[..shift]);
+                f(OuterLayer { pieces: new_pieces });
             }
         }
     }
 
     fn flip(self, other: Self) -> (Self, Self) {
-        let mut new_self_pieces = [OuterPiece::EMPTY; OUTER_LAYER_MAX_PIECES];
-        let mut new_other_pieces = [OuterPiece::EMPTY; OUTER_LAYER_MAX_PIECES];
+        let mut new_self_pieces = self.pieces;
+        let mut new_other_pieces = other.pieces;
 
         // Movable pieces
-        new_self_pieces[..other.movable_pieces]
-            .copy_from_slice(&other.pieces[..other.movable_pieces]);
-        new_other_pieces[..self.movable_pieces]
-            .copy_from_slice(&self.pieces[..self.movable_pieces]);
-
-        // Fixed pieces
-        let self_fixed = self.num_pieces - self.movable_pieces;
-        let other_fixed = other.num_pieces - other.movable_pieces;
-        new_self_pieces[other.movable_pieces..][..self_fixed]
-            .copy_from_slice(&self.pieces[self.movable_pieces..self.num_pieces]);
-        new_other_pieces[self.movable_pieces..][..other_fixed]
-            .copy_from_slice(&other.pieces[other.movable_pieces..other.num_pieces]);
+        new_self_pieces[..OUTER_LAYER_HALF_PIECES]
+            .copy_from_slice(&other.pieces[..OUTER_LAYER_HALF_PIECES]);
+        new_other_pieces[..OUTER_LAYER_HALF_PIECES]
+            .copy_from_slice(&self.pieces[..OUTER_LAYER_HALF_PIECES]);
 
         (
             OuterLayer {
                 pieces: new_self_pieces,
-                num_pieces: other.movable_pieces + self_fixed,
-                movable_pieces: other.movable_pieces,
             },
             OuterLayer {
                 pieces: new_other_pieces,
-                num_pieces: self.movable_pieces + other_fixed,
-                movable_pieces: self.movable_pieces,
             },
         )
     }
@@ -233,33 +98,43 @@ impl OuterLayer {
 
 fn main() {
     let initial_position = Position {
-        top: OuterLayer::new(&[
-            OuterPiece::YELLOW_GREEN_ORANGE,
-            OuterPiece::WHITE_RED,
-            OuterPiece::WHITE_RED_BLUE,
-            OuterPiece::WHITE_BLUE,
-            OuterPiece::YELLOW_BLUE_RED,
-            OuterPiece::YELLOW_RED_GREEN,
-            OuterPiece::WHITE_GREEN,
-            OuterPiece::YELLOW_RED,
+        top: OuterLayer::new([
+            OuterPiece::YellowGreenOrange1,
+            OuterPiece::YellowGreenOrange2,
+            OuterPiece::WhiteRed,
+            OuterPiece::WhiteRedBlue1,
+            OuterPiece::WhiteRedBlue2,
+            OuterPiece::WhiteBlue,
+            OuterPiece::YellowBlueRed1,
+            OuterPiece::YellowBlueRed2,
+            OuterPiece::YellowRedGreen1,
+            OuterPiece::YellowRedGreen2,
+            OuterPiece::WhiteGreen,
+            OuterPiece::YellowRed,
         ]),
-        middle: true,
-        bottom: OuterLayer::new(&[
-            OuterPiece::YELLOW_GREEN,
-            OuterPiece::YELLOW_ORANGE,
-            OuterPiece::YELLOW_ORANGE_BLUE,
-            OuterPiece::YELLOW_BLUE,
-            OuterPiece::WHITE_ORANGE,
-            OuterPiece::WHITE_GREEN_RED,
-            OuterPiece::WHITE_BLUE_ORANGE,
-            OuterPiece::WHITE_ORANGE_GREEN,
+        middle_solved: true,
+        bottom: OuterLayer::new([
+            OuterPiece::YellowGreen,
+            OuterPiece::YellowOrange,
+            OuterPiece::YellowOrangeBlue1,
+            OuterPiece::YellowOrangeBlue2,
+            OuterPiece::YellowBlue,
+            OuterPiece::WhiteOrange,
+            OuterPiece::WhiteGreenRed1,
+            OuterPiece::WhiteGreenRed2,
+            OuterPiece::WhiteBlueOrange1,
+            OuterPiece::WhiteBlueOrange2,
+            OuterPiece::WhiteOrangeGreen1,
+            OuterPiece::WhiteOrangeGreen2,
         ]),
     };
 
     let solved_position = Position::solved();
 
     println!("{}", initial_position);
-    println!("{}", solved_position);
+    initial_position.for_each_movement(|pos| {
+        println!("{}", pos);
+    });
 
     let mut seen_positions = HashSet::new();
     let mut pending_positions = VecDeque::new();
@@ -293,31 +168,18 @@ fn main() {
     eprintln!("start.elapsed() = {:?}", start.elapsed());
 }
 
-impl fmt::Display for MiddleLayer {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                MiddleLayer::Solved => {
-                    "ok"
-                }
-                MiddleLayer::Inverted => {
-                    "inv"
-                }
-            }
-        )
-    }
-}
-
 impl fmt::Display for OuterLayer {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
             "{}|{}",
-            self.pieces[..self.movable_pieces].iter().format(","),
-            self.pieces[..self.num_pieces][self.movable_pieces..]
+            self.pieces[..OUTER_LAYER_HALF_PIECES]
                 .iter()
+                .filter(|p| p.can_split_before())
+                .format(","),
+            self.pieces[OUTER_LAYER_HALF_PIECES..]
+                .iter()
+                .filter(|p| p.can_split_before())
                 .format(",")
         )
     }
@@ -325,68 +187,33 @@ impl fmt::Display for OuterLayer {
 
 impl fmt::Display for OuterPiece {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.name)
-    }
-}
-
-impl fmt::Display for OuterPieceName {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                OuterPieceName::WhiteRedBlue => {
-                    "WRB"
-                }
-                OuterPieceName::WhiteBlue => {
-                    "WB"
-                }
-                OuterPieceName::WhiteBlueOrange => {
-                    "WBO"
-                }
-                OuterPieceName::WhiteOrange => {
-                    "WO"
-                }
-                OuterPieceName::WhiteOrangeGreen => {
-                    "WOG"
-                }
-                OuterPieceName::WhiteGreen => {
-                    "WG"
-                }
-                OuterPieceName::WhiteGreenRed => {
-                    "WGR"
-                }
-                OuterPieceName::WhiteRed => {
-                    "WR"
-                }
-                OuterPieceName::YellowOrange => {
-                    "YO"
-                }
-                OuterPieceName::YellowOrangeBlue => {
-                    "YOB"
-                }
-                OuterPieceName::YellowBlue => {
-                    "YB"
-                }
-                OuterPieceName::YellowBlueRed => {
-                    "YBR"
-                }
-                OuterPieceName::YellowRed => {
-                    "YR"
-                }
-                OuterPieceName::YellowRedGreen => {
-                    "YRG"
-                }
-                OuterPieceName::YellowGreen => {
-                    "YG"
-                }
-                OuterPieceName::YellowGreenOrange => {
-                    "YGO"
-                }
-                OuterPieceName::Empty => {
-                    "*"
-                }
-            }
-        )
+        let str = match self {
+            OuterPiece::WhiteRedBlue1 => "WRB",
+            OuterPiece::WhiteRedBlue2 => "",
+            OuterPiece::WhiteBlue => "WB",
+            OuterPiece::WhiteBlueOrange1 => "WBO",
+            OuterPiece::WhiteBlueOrange2 => "",
+            OuterPiece::WhiteOrange => "WO",
+            OuterPiece::WhiteOrangeGreen1 => "WOG",
+            OuterPiece::WhiteOrangeGreen2 => "",
+            OuterPiece::WhiteGreen => "WG",
+            OuterPiece::WhiteGreenRed1 => "WGR",
+            OuterPiece::WhiteGreenRed2 => "",
+            OuterPiece::WhiteRed => "WR",
+            OuterPiece::YellowOrange => "YO",
+            OuterPiece::YellowOrangeBlue1 => "YOB",
+            OuterPiece::YellowOrangeBlue2 => "",
+            OuterPiece::YellowBlue => "YB",
+            OuterPiece::YellowBlueRed1 => "YBR",
+            OuterPiece::YellowBlueRed2 => "",
+            OuterPiece::YellowRed => "YR",
+            OuterPiece::YellowRedGreen1 => "YRG",
+            OuterPiece::YellowRedGreen2 => "",
+            OuterPiece::YellowGreen => "YG",
+            OuterPiece::YellowGreenOrange1 => "YGO",
+            OuterPiece::YellowGreenOrange2 => "",
+            OuterPiece::Unknown => "*",
+        };
+        write!(f, "{}", str)
     }
 }
--- src/position.rs
+++ src/position.rs
@@ -1,36 +1,44 @@
-use crate::{MiddleLayer, OuterLayer, OuterPiece};
+use crate::{OuterLayer, OuterPiece};
 use std::fmt;
 
 #[derive(Debug, Clone, Copy, Hash, Eq, Ord, PartialOrd, PartialEq)]
 pub struct Position {
     pub top: OuterLayer,
-    pub middle: bool,
+    pub middle_solved: bool,
     pub bottom: OuterLayer,
 }
 
 impl Position {
     pub fn solved() -> Position {
         Position {
-            top: OuterLayer::new(&[
-                OuterPiece::WHITE_RED_BLUE,
-                OuterPiece::WHITE_BLUE,
-                OuterPiece::WHITE_BLUE_ORANGE,
-                OuterPiece::WHITE_ORANGE,
-                OuterPiece::WHITE_ORANGE_GREEN,
-                OuterPiece::WHITE_GREEN,
-                OuterPiece::WHITE_GREEN_RED,
-                OuterPiece::WHITE_RED,
+            top: OuterLayer::new([
+                OuterPiece::WhiteRedBlue1,
+                OuterPiece::WhiteRedBlue2,
+                OuterPiece::WhiteBlue,
+                OuterPiece::WhiteBlueOrange1,
+                OuterPiece::WhiteBlueOrange2,
+                OuterPiece::WhiteOrange,
+                OuterPiece::WhiteOrangeGreen1,
+                OuterPiece::WhiteOrangeGreen2,
+                OuterPiece::WhiteGreen,
+                OuterPiece::WhiteGreenRed1,
+                OuterPiece::WhiteGreenRed2,
+                OuterPiece::WhiteRed,
             ]),
-            middle: true,
-            bottom: OuterLayer::new(&[
-                OuterPiece::YELLOW_ORANGE,
-                OuterPiece::YELLOW_ORANGE_BLUE,
-                OuterPiece::YELLOW_BLUE,
-                OuterPiece::YELLOW_BLUE_RED,
-                OuterPiece::YELLOW_RED,
-                OuterPiece::YELLOW_RED_GREEN,
-                OuterPiece::YELLOW_GREEN,
-                OuterPiece::YELLOW_GREEN_ORANGE,
+            middle_solved: true,
+            bottom: OuterLayer::new([
+                OuterPiece::YellowOrange,
+                OuterPiece::YellowOrangeBlue1,
+                OuterPiece::YellowOrangeBlue2,
+                OuterPiece::YellowBlue,
+                OuterPiece::YellowBlueRed1,
+                OuterPiece::YellowBlueRed2,
+                OuterPiece::YellowRed,
+                OuterPiece::YellowRedGreen1,
+                OuterPiece::YellowRedGreen2,
+                OuterPiece::YellowGreen,
+                OuterPiece::YellowGreenOrange1,
+                OuterPiece::YellowGreenOrange2,
             ]),
         }
     }
@@ -39,7 +47,7 @@ impl Position {
         self.top.for_each_movement(|new_top| {
             f(Position {
                 top: new_top,
-                middle: self.middle,
+                middle_solved: self.middle_solved,
                 bottom: self.bottom,
             })
         });
@@ -47,14 +55,14 @@ impl Position {
         let (flipped_top, flipped_bottom) = self.top.flip(self.bottom);
         f(Position {
             top: flipped_top,
-            middle: !self.middle,
+            middle_solved: !self.middle_solved,
             bottom: flipped_bottom,
         });
 
         self.bottom.for_each_movement(|new_bottom| {
             f(Position {
                 top: self.top,
-                middle: self.middle,
+                middle_solved: self.middle_solved,
                 bottom: new_bottom,
             })
         });
@@ -63,6 +71,6 @@ impl Position {
 
 impl fmt::Display for Position {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {} {}", self.top, self.middle, self.bottom)
+        write!(f, "{} {} {}", self.top, self.middle_solved, self.bottom)
     }
 }
