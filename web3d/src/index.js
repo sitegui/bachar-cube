e88eb0c396cc831b8c06395e1c8fe587f06b2450
sitegui <sitegui@sitegui.com.br>
2022-05-28 01:14:16 +0200

    Some 3d experiments

---
+++ web3d/src/index.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 10;
+
+// controls
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 10;
+controls.maxDistance = 20;
+controls.maxPolarAngle = Math.PI / 2;
+
+// ambient light
+
+scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+// point light
+
+const light = new THREE.PointLight( 0xffffff, 1 );
+camera.add( light );
+
+// helper
+
+scene.add( new THREE.AxesHelper( 20 ) );
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+animate();
