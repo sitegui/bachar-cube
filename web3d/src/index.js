21fa477ec2fdf459a1c462e8b2301012c96b23cb
sitegui <sitegui@sitegui.com.br>
2022-05-29 18:19:24 +0200

    I have a cube :)

--- web3d/src/index.js
+++ web3d/src/index.js
@@ -1,103 +1,244 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const material1 = new THREE.MeshStandardMaterial({color: 'green'});
-const material2 = new THREE.MeshStandardMaterial({color: 'red'});
+class Cube {
+    constructor() {
+        const cubeSide = 1;
+        const smallPieceAngle = Math.PI / 6;
+        const halfSmallPieceSide = cubeSide * Math.tan(smallPieceAngle / 2) / 2;
+        const pieceHorizontalBevel = cubeSide / 20;
+        const pieceVerticalBevel = cubeSide / 15;
 
-const cubeSide = 1;
+        const pieceMaterial = new THREE.MeshStandardMaterial({color: '#333333', roughness: 0.25, metalness: 0});
+        const whiteMaterial = new THREE.MeshStandardMaterial({color: 'white'});
+        const redMaterial = new THREE.MeshStandardMaterial({color: 'red'});
+        const blueMaterial = new THREE.MeshStandardMaterial({color: 'blue'});
+        const orangeMaterial = new THREE.MeshStandardMaterial({color: 'orange'});
+        const greenMaterial = new THREE.MeshStandardMaterial({color: 'green'});
+        const yellowMaterial = new THREE.MeshStandardMaterial({color: 'yellow'});
 
-const smallPieceAngle = Math.PI / 6;
-const halfSmallPieceSide = cubeSide * Math.tan(smallPieceAngle / 2) / 2;
-const halfCubeSide = cubeSide / 2;
+        function buildSmallPiece(bottomFaceMaterial, topFaceMaterial, sideFaceMaterial, rotateZ, translateZ) {
+            const piece = new SmallPiece(halfSmallPieceSide, cubeSide, pieceVerticalBevel, pieceHorizontalBevel, pieceMaterial, bottomFaceMaterial, topFaceMaterial, sideFaceMaterial);
+            piece.mesh.rotateZ(rotateZ);
+            piece.mesh.translateZ(translateZ);
+            return piece;
+        }
 
-function buildSmallPiece(material, topFaceMaterial, sideFaceMaterial) {
-    const topShape = new THREE.Shape();
-    topShape.moveTo(0, 0);
-    topShape.lineTo(-halfSmallPieceSide, -halfCubeSide);
-    topShape.lineTo(halfSmallPieceSide, -halfCubeSide);
-    topShape.closePath();
+        function buildBigPiece(bottomFaceMaterial, topFaceMaterial, sideYFaceMaterial, sideXFaceMaterial, rotateZ, translateZ) {
+            const piece = new BigPiece(halfSmallPieceSide, cubeSide, pieceVerticalBevel, pieceHorizontalBevel, pieceMaterial, bottomFaceMaterial, topFaceMaterial, sideYFaceMaterial, sideXFaceMaterial);
+            piece.mesh.rotateZ(rotateZ);
+            piece.mesh.translateZ(translateZ);
+            return piece;
+        }
 
-    const pieceGroup = new THREE.Group();
+        function buildMiddlePiece(frontFaceMaterial, sideFaceMaterial, backFaceMaterial, rotateZ) {
+            const piece = new MiddlePiece(halfSmallPieceSide, cubeSide, pieceVerticalBevel, pieceHorizontalBevel, pieceMaterial, frontFaceMaterial, sideFaceMaterial, backFaceMaterial);
+            piece.mesh.rotateZ(rotateZ);
+            piece.mesh.translateZ(-cubeSide / 6);
+            return piece;
+        }
 
-    const pieceGeometry = new THREE.ExtrudeGeometry(topShape, {
-        depth: cubeSide / 3,
-        bevelSize: 0,
-        bevelThickness: 0,
-    });
-    const piece = new THREE.Mesh(pieceGeometry, material);
-    pieceGroup.add(piece);
+        this.pieces = [
+            // Top pieces
+            buildBigPiece(pieceMaterial, whiteMaterial, redMaterial, blueMaterial, 0, cubeSide / 6),
+            buildSmallPiece(pieceMaterial, whiteMaterial, blueMaterial, 3 * smallPieceAngle, cubeSide / 6),
+            buildBigPiece(pieceMaterial, whiteMaterial, blueMaterial, orangeMaterial, 3 * smallPieceAngle, cubeSide / 6),
+            buildSmallPiece(pieceMaterial, whiteMaterial, orangeMaterial, 6 * smallPieceAngle, cubeSide / 6),
+            buildBigPiece(pieceMaterial, whiteMaterial, orangeMaterial, greenMaterial, 6 * smallPieceAngle, cubeSide / 6),
+            buildSmallPiece(pieceMaterial, whiteMaterial, greenMaterial, 9 * smallPieceAngle, cubeSide / 6),
+            buildBigPiece(pieceMaterial, whiteMaterial, greenMaterial, redMaterial, 9 * smallPieceAngle, cubeSide / 6),
+            buildSmallPiece(pieceMaterial, whiteMaterial, redMaterial, 12 * smallPieceAngle, cubeSide / 6),
+            // Middle pieces
+            buildMiddlePiece(redMaterial, blueMaterial, orangeMaterial, 0),
+            buildMiddlePiece(orangeMaterial, greenMaterial, redMaterial, Math.PI),
+            // Bottom pieces
+            buildBigPiece(yellowMaterial, pieceMaterial, redMaterial, blueMaterial, 0, -cubeSide / 2),
+            buildSmallPiece(yellowMaterial, pieceMaterial, blueMaterial, 3 * smallPieceAngle, -cubeSide / 2),
+            buildBigPiece(yellowMaterial, pieceMaterial, blueMaterial, orangeMaterial, 3 * smallPieceAngle, -cubeSide / 2),
+            buildSmallPiece(yellowMaterial, pieceMaterial, orangeMaterial, 6 * smallPieceAngle, -cubeSide / 2),
+            buildBigPiece(yellowMaterial, pieceMaterial, orangeMaterial, greenMaterial, 6 * smallPieceAngle, -cubeSide / 2),
+            buildSmallPiece(yellowMaterial, pieceMaterial, greenMaterial, 9 * smallPieceAngle, -cubeSide / 2),
+            buildBigPiece(yellowMaterial, pieceMaterial, greenMaterial, redMaterial, 9 * smallPieceAngle, -cubeSide / 2),
+            buildSmallPiece(yellowMaterial, pieceMaterial, redMaterial, 12 * smallPieceAngle, -cubeSide / 2),
+        ];
 
-    const topFaceGeometry = new THREE.ShapeGeometry(topShape);
-    const topFace = new THREE.Mesh(topFaceGeometry, topFaceMaterial);
-    topFace.position.z = cubeSide / 3 * 1.001;
-    pieceGroup.add(topFace);
-
-    const sideFaceGeometry = new THREE.PlaneGeometry(2 * halfSmallPieceSide, cubeSide / 3);
-    const sideFace = new THREE.Mesh(sideFaceGeometry, sideFaceMaterial);
-    sideFace.rotateX(Math.PI / 2);
-    sideFace.translateZ(halfCubeSide * 1.001);
-    sideFace.translateY(halfCubeSide / 3);
-    pieceGroup.add(sideFace);
-
-    return pieceGroup;
+        this.mesh = new THREE.Group();
+        for (const piece of this.pieces) {
+            this.mesh.add(piece.mesh);
+        }
+    }
 }
 
-function buildBigPiece(material, topFaceMaterial, sideXFaceMaterial, sideYFaceMaterial) {
-    const topShape = new THREE.Shape();
-    topShape.moveTo(0, 0);
-    topShape.lineTo(halfSmallPieceSide, -halfCubeSide);
-    topShape.lineTo(halfCubeSide, -halfCubeSide);
-    topShape.lineTo(halfCubeSide, -halfSmallPieceSide);
-    topShape.closePath();
+class SmallPiece {
+    constructor(halfSide, cubeSide, verticalBevel, horizontalBevel, mainMaterial, bottomFaceMaterial, topFaceMaterial, sideFaceMaterial) {
+        const halfCubeSide = cubeSide / 2;
+        const shape = new THREE.Shape();
+        shape.moveTo(0, 0);
+        shape.lineTo(-halfSide, -halfCubeSide);
+        shape.lineTo(halfSide, -halfCubeSide);
+        shape.closePath();
 
-    const pieceGroup = new THREE.Group();
+        const group = new THREE.Group();
 
-    const pieceGeometry = new THREE.ExtrudeGeometry(topShape, {
-        depth: cubeSide / 3,
-        bevelSize: 0,
-        bevelThickness: 0,
-    });
-    const piece = new THREE.Mesh(pieceGeometry, material);
-    pieceGroup.add(piece);
+        const sideHeight = cubeSide / 3 - 2 * verticalBevel;
+        const geometry = new THREE.ExtrudeGeometry(shape, {
+            depth: sideHeight,
+            bevelSize: horizontalBevel,
+            bevelOffset: -horizontalBevel,
+            bevelThickness: verticalBevel,
+        });
 
-    const topFaceGeometry = new THREE.ShapeGeometry(topShape);
-    const topFace = new THREE.Mesh(topFaceGeometry, topFaceMaterial);
-    topFace.position.z = cubeSide / 3 * 1.001;
-    pieceGroup.add(topFace);
+        // Make three groups out of the two ordinary ones (faces, side) -> (bottom, top, side)
+        const geometryGroups = geometry.groups;
+        geometry.clearGroups();
+        const halfFirstGroupCount = geometryGroups[0].count / 2;
+        geometry.addGroup(0, halfFirstGroupCount, 0);
+        geometry.addGroup(halfFirstGroupCount, halfFirstGroupCount, 1);
+        geometry.addGroup(2 * halfFirstGroupCount, geometryGroups[1].count, 2);
 
-    return pieceGroup;
+        const piece = new THREE.Mesh(geometry, [bottomFaceMaterial, topFaceMaterial, mainMaterial]);
+        piece.translateZ(verticalBevel);
+        group.add(piece);
+
+        const sideFaceGeometry = new THREE.PlaneGeometry(2 * halfSide - 2 * horizontalBevel, sideHeight);
+        const sideFace = new THREE.Mesh(sideFaceGeometry, sideFaceMaterial);
+        sideFace.rotateX(Math.PI / 2);
+        sideFace.translateZ(halfCubeSide * 1.001);
+        sideFace.translateY(halfCubeSide / 3);
+        group.add(sideFace);
+
+        this.mesh = group;
+    }
 }
 
-const cube = new THREE.Group();
+class BigPiece {
+    constructor(halfSmallPieceSide, cubeSide, verticalBevel, horizontalBevel, mainMaterial, bottomFaceMaterial, topFaceMaterial, sideYFaceMaterial, sideXFaceMaterial) {
+        const halfCubeSide = cubeSide / 2;
+        const shape = new THREE.Shape();
+        shape.moveTo(0, 0);
+        shape.lineTo(halfSmallPieceSide, -halfCubeSide);
+        shape.lineTo(halfCubeSide, -halfCubeSide);
+        shape.lineTo(halfCubeSide, -halfSmallPieceSide);
+        shape.closePath();
 
-const pieceMaterial = new THREE.MeshStandardMaterial({color: 'blue'});
-const whiteMaterial = new THREE.MeshStandardMaterial({color: 'white'});
-const redMaterial = new THREE.MeshStandardMaterial({color: 'red'});
-const greenMaterial = new THREE.MeshStandardMaterial({color: 'green'});
+        const group = new THREE.Group();
 
-cube.add(buildSmallPiece(pieceMaterial, whiteMaterial, redMaterial));
-cube.add(buildBigPiece(pieceMaterial, whiteMaterial, redMaterial, greenMaterial));
+        const sideHeight = cubeSide / 3 - 2 * verticalBevel;
+        const geometry = new THREE.ExtrudeGeometry(shape, {
+            depth: sideHeight,
+            bevelSize: horizontalBevel,
+            bevelOffset: -horizontalBevel,
+            bevelThickness: verticalBevel,
+        });
 
-scene.add(cube);
+        // Make three groups out of the two ordinary ones (faces, side) -> (bottom, top, side)
+        const geometryGroups = geometry.groups;
+        geometry.clearGroups();
+        const halfFirstGroupCount = geometryGroups[0].count / 2;
+        geometry.addGroup(0, halfFirstGroupCount, 0);
+        geometry.addGroup(halfFirstGroupCount, halfFirstGroupCount, 1);
+        geometry.addGroup(2 * halfFirstGroupCount, geometryGroups[1].count, 2);
 
-camera.position.z = 4;
+        const piece = new THREE.Mesh(geometry, [bottomFaceMaterial, topFaceMaterial, mainMaterial]);
+        piece.translateZ(verticalBevel);
+        group.add(piece);
+
+        const sideWidth = halfCubeSide - halfSmallPieceSide - 2 * horizontalBevel;
+        const sideFaceGeometry = new THREE.PlaneGeometry(sideWidth, sideHeight);
+
+        const sideYFace = new THREE.Mesh(sideFaceGeometry, sideYFaceMaterial);
+        sideYFace.rotateX(Math.PI / 2);
+        sideYFace.translateZ(halfCubeSide * 1.001);
+        sideYFace.translateY(halfCubeSide / 3);
+        sideYFace.translateX(halfSmallPieceSide + sideWidth / 2 + horizontalBevel);
+        group.add(sideYFace);
+
+        const sideXFace = new THREE.Mesh(sideFaceGeometry, sideXFaceMaterial);
+        sideXFace.rotateZ(Math.PI / 2);
+        sideXFace.rotateX(Math.PI / 2);
+        sideXFace.translateZ(halfCubeSide * 1.001);
+        sideXFace.translateY(halfCubeSide / 3);
+        sideXFace.translateX(-halfSmallPieceSide - sideWidth / 2 - horizontalBevel);
+        group.add(sideXFace);
+
+        this.mesh = group;
+    }
+}
+
+class MiddlePiece {
+    constructor(halfSmallPieceSide, cubeSide, verticalBevel, horizontalBevel, mainMaterial, frontFaceMaterial, sideFaceMaterial, backFaceMaterial) {
+        const halfCubeSide = cubeSide / 2;
+        const shape = new THREE.Shape();
+        shape.moveTo(halfSmallPieceSide, -halfCubeSide);
+        shape.lineTo(halfCubeSide, -halfCubeSide);
+        shape.lineTo(halfCubeSide, halfCubeSide);
+        shape.lineTo(-halfSmallPieceSide, halfCubeSide);
+        shape.closePath();
+
+        const group = new THREE.Group();
+
+        const sideHeight = cubeSide / 3 - 2 * verticalBevel;
+        const geometry = new THREE.ExtrudeGeometry(shape, {
+            depth: sideHeight,
+            bevelSize: horizontalBevel,
+            bevelOffset: -horizontalBevel,
+            bevelThickness: verticalBevel,
+        });
+
+        const piece = new THREE.Mesh(geometry, mainMaterial);
+        piece.translateZ(verticalBevel);
+        group.add(piece);
+
+        const frontWidth = halfCubeSide - halfSmallPieceSide - 2 * horizontalBevel;
+        const frontFace = new THREE.Mesh(new THREE.PlaneGeometry(frontWidth, sideHeight), frontFaceMaterial);
+        frontFace.rotateX(Math.PI / 2);
+        frontFace.translateZ(halfCubeSide * 1.001);
+        frontFace.translateY(halfCubeSide / 3);
+        frontFace.translateX(halfSmallPieceSide + frontWidth / 2 + horizontalBevel);
+        group.add(frontFace);
+
+        const sideWidth = cubeSide - 2 * horizontalBevel;
+        const sideFace = new THREE.Mesh(new THREE.PlaneGeometry(sideWidth, sideHeight), sideFaceMaterial);
+        sideFace.rotateZ(Math.PI / 2);
+        sideFace.rotateX(Math.PI / 2);
+        sideFace.translateZ(halfCubeSide * 1.001);
+        sideFace.translateY(halfCubeSide / 3);
+        group.add(sideFace);
+
+        const backWidth = halfCubeSide + halfSmallPieceSide - 2 * horizontalBevel;
+        const backFace = new THREE.Mesh(new THREE.PlaneGeometry(backWidth, sideHeight), backFaceMaterial);
+        backFace.rotateX(-Math.PI / 2);
+        backFace.translateZ(halfCubeSide * 1.001);
+        backFace.translateY(-halfCubeSide / 3);
+        backFace.translateX(halfCubeSide - backWidth / 2 - horizontalBevel);
+        group.add(backFace);
+
+        this.mesh = group;
+    }
+}
+
+const cube = new Cube();
+scene.add(cube.mesh);
+
+camera.position.z = 3;
 
 // controls
 
-// const controls = new OrbitControls( camera, renderer.domElement );
-// controls.minDistance = 10;
-// controls.maxDistance = 20;
-// controls.maxPolarAngle = Math.PI / 2;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 0.5;
+controls.maxDistance = 3;
+controls.maxPolarAngle = Math.PI;
 
 // ambient light
 
-// scene.add( new THREE.AmbientLight( 0x222222 ) );
+scene.add(new THREE.AmbientLight('white'));
 
 // point light
 
@@ -108,18 +249,22 @@ camera.position.z = 4;
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    light.position.set(-1, 2, 6);
     scene.add(light);
+
+    const light2 = new THREE.DirectionalLight(color, intensity);
+    light2.position.set(1, -2, 6);
+    // scene.add(light2);
 }
 
 // helper
 
-scene.add(new THREE.AxesHelper(20));
+cube.mesh.add(new THREE.AxesHelper(20));
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x -= 0.01;
-    cube.rotation.y -= 0.01;
+    cube.mesh.rotation.z -= 0.01;
+    cube.mesh.rotation.x -= 0.003;
     renderer.render(scene, camera);
 }
 
