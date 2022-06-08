9bc287a3fe2145ab0951a42ba7e90d8c403c5125
sitegui <sitegui@sitegui.com.br>
2022-06-08 16:58:47 +0200

    Stir bits when inserting in prefix set
    Also increase prefix size

--- src/prefix_set.rs
+++ src/prefix_set.rs
@@ -1,49 +1,52 @@
+use itertools::Itertools;
 use parking_lot::Mutex;
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 use std::mem;
-use std::mem::MaybeUninit;
 
 /// Represent the set of all possible [`u64`].
 #[derive(Debug)]
 pub struct PrefixSet {
-    children: [Mutex<HashSet<u64>>; 256],
+    children: Vec<Mutex<HashSet<u64>>>,
 }
+
+const PREFIX_BITS: u32 = 16;
 
 impl PrefixSet {
     pub fn new() -> Self {
-        // Taken from https://doc.rust-lang.org/std/mem/union.MaybeUninit.html
-        let children = {
-            // Create an uninitialized array of `MaybeUninit`. The `assume_init` is
-            // safe because the type we are claiming to have initialized here is a
-            // bunch of `MaybeUninit`s, which do not require initialization.
-            let mut children: [MaybeUninit<Mutex<HashSet<u64>>>; 256] =
-                unsafe { MaybeUninit::uninit().assume_init() };
-
-            // Dropping a `MaybeUninit` does nothing. Thus using raw pointer
-            // assignment instead of `ptr::write` does not cause the old
-            // uninitialized value to be dropped. Also if there is a panic during
-            // this loop, we have a memory leak, but there is no memory safety
-            // issue.
-            for elem in &mut children[..] {
-                elem.write(Mutex::new(HashSet::new()));
-            }
-
-            // Everything is initialized. Transmute the array to the
-            // initialized type.
-            unsafe { mem::transmute::<_, [Mutex<HashSet<u64>>; 256]>(children) }
-        };
-
-        PrefixSet { children }
+        PrefixSet {
+            children: (0..2u32.pow(PREFIX_BITS))
+                .map(|_| Mutex::new(HashSet::new()))
+                .collect_vec(),
+        }
     }
 
     pub fn insert(&self, value: u64) -> bool {
-        let first_byte = value >> 56;
-        self.children[first_byte as usize]
-            .lock()
-            .insert(value ^ (first_byte << 56))
+        let value = stir(value);
+        let prefix = value >> (u64::BITS - PREFIX_BITS);
+        let suffix = value ^ (prefix << (u64::BITS - PREFIX_BITS));
+        self.children[prefix as usize].lock().insert(suffix)
     }
 
     pub fn len(&self) -> usize {
         self.children.iter().map(|child| child.lock().len()).sum()
     }
+}
+
+fn stir(v: u64) -> u64 {
+    let [mut a, mut b, mut c, mut d] = unsafe { mem::transmute::<u64, [u16; 4]>(v) };
+
+    for _ in 0..4 {
+        // Slightly inspired by the core of SHA-1
+        let new_a = (b ^ c).wrapping_add(d).wrapping_add(a.rotate_left(5));
+        let new_b = a;
+        let new_c = b.rotate_left(14);
+        let new_d = c;
+
+        a = new_a;
+        b = new_b;
+        c = new_c;
+        d = new_d;
+    }
+
+    unsafe { mem::transmute::<[u16; 4], u64>([a, b, c, d]) }
 }
